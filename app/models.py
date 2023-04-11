0c183458330bc4e7a3aad0607bdc475506eaca4d
Kimberly Laughton <kimberlyclaughton@gmail.com>
2023-04-11 15:54:02 -0500

    Completed exercise 2

--- app/models.py
+++ app/models.py
@@ -1,1 +1,18 @@
-# Add any model classes for Flask-SQLAlchemy here
+# Add any model classes for Flask-SQLAlchemy herefrom . import db
+from datetime import datetime
+from . import db
+class Movie(db.Model):
+    __tablename__ = 'movies'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String(200))
+    description = db.Column(db.Text())
+    poster = db.Column(db.String())
+    
+    def __init__(self, title, description, poster):
+        self.title = title
+        self.description = description
+        self.poster = poster
+
+    def __repr__(self):
+        return '<Movie %r>' % (self.title)
